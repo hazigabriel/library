1699abff5bdc4bb11d7ae020c6a75ccb0360aa56
Gabriel Hazi <hazigabriel@gmail.com>
2020-06-29 22:39:34 +0300

    started adding a modal, to receive user input for new books

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
- let click = document.querySelector(".far");
+ let click = document.querySelector(".fas");
  click.addEventListener("click", function(){
- 	alert()
+ 	addBookToLibray()
  })
+
+let myLibrary = [];
+
+function Book(title, author, pageNo, readStatus) {
+	this.title = title;
+	this.author = author;
+	this.pageNo = pageNo;
+	this.readStatus = readStatus
+}
+
+
+function addBookToLibray() {
+	let one = prompt("Ce title ie cartea");
+	let two = prompt("Ce autor ie cartea");
+	let three = prompt("Ce page ie cartea");
+	let four = prompt("Ce citit ie cartea");
+	myLibrary.push(new Book(one, two, three, four))
+}
